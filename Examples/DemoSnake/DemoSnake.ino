8951939a365e2de1e7804bb4f4f90d897c3b6d07
joshua salinas zarco <salinaszarcojoshua@gmail.com>
2026-06-01 14:26:50 -0600

    mod 5

--- Examples/DemoSnake/DemoSnake.ino
+++ Examples/DemoSnake/DemoSnake.ino
@@ -2,18 +2,58 @@
 
 Snake juego;
 
+#define BTN_UP     2
+#define BTN_DOWN   3
+#define BTN_LEFT   4
+#define BTN_RIGHT  5
+
 void setup() {
 
   Matrix::begin();
 
-  randomSeed(
-    analogRead(A0)
-  );
+  pinMode(BTN_UP, INPUT_PULLUP);
+  pinMode(BTN_DOWN, INPUT_PULLUP);
+  pinMode(BTN_LEFT, INPUT_PULLUP);
+  pinMode(BTN_RIGHT, INPUT_PULLUP);
+
+  randomSeed(analogRead(A0));
 
   juego.begin();
 }
 
 void loop() {
 
+  if(digitalRead(BTN_UP) == LOW) {
+
+    juego.setDirection(
+      0,
+      -1
+    );
+  }
+
+  if(digitalRead(BTN_DOWN) == LOW) {
+
+    juego.setDirection(
+      0,
+      1
+    );
+  }
+
+  if(digitalRead(BTN_LEFT) == LOW) {
+
+    juego.setDirection(
+      -1,
+      0
+    );
+  }
+
+  if(digitalRead(BTN_RIGHT) == LOW) {
+
+    juego.setDirection(
+      1,
+      0
+    );
+  }
+
   juego.update();
-}
+} 
